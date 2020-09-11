9c66248c6852aeb81f483c36217e4a134ddf2ec6
ahooee <ahooyee@gmail.com>
2020-09-11 16:36:39 +0430

    شروع کار

---
+++ app/src/main/java/ir/linuxian/sql/activityha/AghazActivity.java
@@ -0,0 +1,17 @@
+package ir.linuxian.sql.activityha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.linuxian.sql.R;
+
+public class AghazActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
